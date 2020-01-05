439ad007d45b1a0103045257413098ef828b7747
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-05 01:09:21 +0100

    clean-up 🥑 ..

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-
 const https = require('https');
 const semver = require('semver');
 
 
 
-  /**
-   * 
-   * Through this function you get a list of your package input name..
-   * @param {String} PackageName : The name of the npm package ..
-   * @return {Array} 
-   * @public
-   * 
-   */
-
-
+/**
+ * 
+ * Through this function you get a list of your package input name..
+ * @param {String} PackageName : The name of the npm package ..
+ * @return {Array} 
+ * @public
+ * 
+ */
 module.exports = PackageName => new Promise((resolve, reject) => {
 
     https.request({
-          method: 'GET',
-          host: 'registry.npmjs.org',
-          path: '/' + PackageName,
-          headers: { 'Content-Type': 'application/json' },
-          agent: false,
-        }, response => {
+        method: 'GET',
+        host: 'registry.npmjs.org',
+        path: '/' + PackageName,
+        headers: { 'Content-Type': 'application/json' },
+        agent: false,
+      },
+      response => {
           
-      if (response.statusCode !== 200) {
-        return reject(new Error(`Failed to get npmjs.org/${PackageName} version(s).`).message);
-      }     
+        if (response.statusCode !== 200) {
+          return reject(new Error(`Failed to get npmjs.org/${PackageName} version(s).`).message);
+        }     
  
                     
         let data = '';
         response.setEncoding('utf8');
         response.on('data', (letter) => { data += letter });
-        response.once('end',() => resolve(
-                                    Object
-                                        .keys(JSON.parse(data).versions)
-                                        .filter( version => semver.valid(version))
-                                        .sort(semver.rcompare)
-                                  ));     
+        response.once(
+          'end', 
+          () => resolve(
+                  Object
+                    .keys(JSON.parse(data).versions)
+                    .filter( version => semver.valid(version))
+                    .sort(semver.rcompare)
+        ));     
     })
-
     .on('error', error => reject(error.message))
-    // send the request
     .end();
 
   });
