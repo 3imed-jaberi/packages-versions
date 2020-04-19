a9997226a2bd9eed9c4b2f057f8ab0d95478be57
3imed-jaberi <imed_jebari@hotmail.fr>
2020-04-19 02:04:15 +0200

    add new feature --opts 🚀 ..

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
 const https = require('https');
 const semver = require('semver');
 
-
-
 /**
- * 
  * Through this function you get a list of your package input name..
- * @param {String} PackageName : The name of the npm package ..
- * @return {Array} 
- * @public
- * 
+ * @param {String} PackageName: name of the npm package .. 
+ * @param {Object} Opts: options for foramt result ..
  */
-module.exports = PackageName => new Promise((resolve, reject) => {
+module.exports = function (PackageName, Opts) {
+  // TODO: add more options like filter/extract ..
+  Opts = Opts || {};
+  const reverse = Opts.reverse || false;
 
+  return new Promise((resolve, reject) => {
     https.request({
         method: 'GET',
         host: 'registry.npmjs.org',
-        path: '/' + PackageName,
+        path:  `/${PackageName}`,
         headers: { 'Content-Type': 'application/json' },
         agent: false,
       },
       response => {
-          
+
         if (response.statusCode !== 200) {
           return reject(new Error(`Failed to get npmjs.org/${PackageName} version(s).`).message);
         }     
- 
-                    
+
+     
         let data = '';
         response.setEncoding('utf8');
         response.on('data', (letter) => { data += letter });
         response.once(
           'end', 
-          () => resolve(
-                  Object
-                    .keys(JSON.parse(data).versions)
-                    .filter( version => semver.valid(version))
-                    .sort(semver.rcompare)
-        ));     
+          () => {
+            // TODO: make this part more better for put more opts.
+            const ResponseData = Object
+                            .keys(JSON.parse(data).versions)
+                            .filter(version => semver.valid(version))
+                            .sort(semver.rcompare);
+            const result = reverse ? ResponseData.reverse() : ResponseData;
+            
+            resolve(result);
+          }
+        );
     })
+    // TODO: think about custom error .. 
     .on('error', error => reject(error.message))
     .end();
-
   });
+}
